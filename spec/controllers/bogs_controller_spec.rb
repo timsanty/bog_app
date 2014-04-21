947355e93bd0975e21b824875b5d1ee08f93aeb6
Delmer <delmer814@yahoo.com>
2014-04-21 14:53:44 -0700

    first

---
+++ spec/controllers/bogs_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe BogsController do
+  describe "GET #index" do
+  end
+
+  describe "GET #new" do
+  end
+
+  describe "POST #create" do
+  end
+
+end
---
+++ spec/models/bog_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Bog do
+  describe "attributes" do
+    it "should have a name" do
+    end
+
+    it "should have a description" do
+    end
+  end
+end
